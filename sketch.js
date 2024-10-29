224d3e6d8632d9ac9798ee18272b255371d214e9
chazlio <96962703+chazlio@users.noreply.github.com>
2024-10-29 18:41:20 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+/**
+ * Claire Wang, 10/29/24
+ * wangworks
+ * my website
+ */
+
+"use strict"; //catch some common coding errors
+
+/* Global variables */
+
+/**
+ * setup :
+ */
+function setup() {
+   createCanvas(400, 400);
+   background("yellow")
+}
+
+/**
+ * draw :
+ */
+function draw() {
+rect(200, 200, 100)
+ellipse(100, 100, 100)
+}
